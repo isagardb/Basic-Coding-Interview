69ef72b2178b01cc367590312883eb3fe5942dcc
Sagar DB (DropCodes) <sagardb50@gmail.com>
2019-09-23 19:19:35 +0530

    Create LeapYear.java

---
+++ src/LeapYear.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class LeapYear {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		boolean leap = checkLeap(year);
+		
+		if(leap == true) {
+			System.out.print("Leap year");
+		}else {
+			System.out.print("Not Leap year");
+		}
+				
+	}
+	
+	public static boolean checkLeap(int year) {
+		
+		if(year % 4 == 0) {
+			if(year % 100 == 0) {
+				if(year % 400 == 0) {
+					return true;
+				}else {
+					return false;
+				}
+			}else {
+				return true;
+			}
+		}else {
+			return false;
+		}
+	}
+}
